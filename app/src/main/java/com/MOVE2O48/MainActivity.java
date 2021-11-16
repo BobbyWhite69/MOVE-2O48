7ad2e7224d43bb716beff660a4bcfdcbf3af40d6
BobbyWhite69 <alessandro.pisani@edu.tbz.ch>
2021-11-16 14:54:20 +0100

    imported profile + created all views

---
+++ app/src/main/java/com/MOVE2O48/MainActivity.java
@@ -0,0 +1,14 @@
+package com.MOVE2O48;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_Start);
+    }
+}
